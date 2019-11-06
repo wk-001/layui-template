3164ea3b4da18aec3bc1adf327e064fca56b64b7
wk-001 <2326699650@qq.com>
2019-11-06 17:40:04 +0800

    Initial commit

---
+++ src/main/java/com/wk/LayuiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayuiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayuiTemplateApplication.class, args);
+	}
+
+}
